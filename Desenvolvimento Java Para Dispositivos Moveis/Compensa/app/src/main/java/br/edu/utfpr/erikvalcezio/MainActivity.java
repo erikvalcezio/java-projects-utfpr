f5bbd8f3d910165771c7dac750b0a65f9c02d825
Erik Valcezio <valcezio@gmail.com>
2022-02-08 20:26:17 -0300

    Android projeto Compensa - tela de cadastro de produto

--- Desenvolvimento Java Para Dispositivos Moveis/Compensa/app/src/main/java/br/edu/utfpr/erikvalcezio/MainActivity.java
+++ Desenvolvimento Java Para Dispositivos Moveis/Compensa/app/src/main/java/br/edu/utfpr/erikvalcezio/MainActivity.java
@@ -4,28 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView descricaoProduto, preco, quantidade;
+    private TextView descricaoProduto, preco, quantidade, total;
+    private RadioGroup unidadeMedida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //TextView
         this.descricaoProduto = findViewById(R.id.editTextTextPersonNameDescricaoProduto);
         this.preco = findViewById(R.id.editTextTextPersonNamePreco);
         this.quantidade = findViewById(R.id.editTextTextPersonNameQuantidade);
+        this.total = findViewById(R.id.editTextNumberDecimalTotal);
+
+        //RadioGroup
+        this.unidadeMedida = findViewById(R.id.radioGroupUnidadeMedida);
+
     }
 
     public void limparCampos(View view){
         this.descricaoProduto.setText("");
         this.preco.setText("");
         this.quantidade.setText("");
+        this.total.setText("");
+        this.unidadeMedida.clearCheck();
 
         this.descricaoProduto.hasFocus();
+    }
+
+    public void calcularTotal(View View) {
+        this.total.setText("");
+        Integer qtd = 0;
+        BigDecimal price = BigDecimal.ZERO;
+        BigDecimal somaTotal = BigDecimal.ZERO;
+
+        if (this.quantidade.getText().toString().trim() != null
+            && this.preco.getText().toString().trim()  != null ) {
+            qtd =  Integer.parseInt(this.quantidade.getText().toString().trim());
+            price =  new BigDecimal(this.preco.getText().toString().trim());
+            somaTotal = price.multiply(new BigDecimal(qtd));
+            this.total.setText(somaTotal.toString());
+        }
     }
 
 }
