0a1efb76bc76e0ad3a1847f9c4861b5f676da9a9
Erik Valcezio <valcezio@gmail.com>
2022-02-08 03:29:03 -0300

    Android projeto Compensa

--- Desenvolvimento Java Para Dispositivos Moveis/Compensa/app/src/main/java/br/edu/utfpr/erikvalcezio/MainActivity.java
+++ Desenvolvimento Java Para Dispositivos Moveis/Compensa/app/src/main/java/br/edu/utfpr/erikvalcezio/MainActivity.java
@@ -3,12 +3,27 @@ package br.edu.utfpr.erikvalcezio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView descricaoProduto, preco, quantidade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.descricaoProduto = findViewById(R.id.editTextTextPersonNameDescricaoProduto);
+        this.preco = findViewById(R.id.editTextTextPersonNamePreco);
+        this.quantidade = findViewById(R.id.editTextTextPersonNameQuantidade);
     }
+
+    public void limparCampos(View view){
+        this.descricaoProduto.setText("");
+        this.preco.setText("");
+        this.quantidade.setText("");
+    }
+
 }
