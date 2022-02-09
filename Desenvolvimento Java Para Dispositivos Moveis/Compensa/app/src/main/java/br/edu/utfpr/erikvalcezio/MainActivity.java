6bc5e64b7c74d229e2d798f3d1f1a05bf04628e8
Erik Valcezio <valcezio@gmail.com>
2022-02-09 02:31:15 -0300

    Android projeto Compensa - tela de cadastro de produto

--- Desenvolvimento Java Para Dispositivos Moveis/Compensa/app/src/main/java/br/edu/utfpr/erikvalcezio/MainActivity.java
+++ Desenvolvimento Java Para Dispositivos Moveis/Compensa/app/src/main/java/br/edu/utfpr/erikvalcezio/MainActivity.java
@@ -1,20 +1,18 @@
 package br.edu.utfpr.erikvalcezio;
 
-import static java.util.Arrays.asList;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup unidadeMedidaRadioGroup;
     private Spinner setorSpinner;
     private CheckBox favoritoCheckBox;
+    private int quantidadeProdutos;
+    private BigDecimal precoProduto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
         this.quantidadeText = findViewById(R.id.editTextTextPersonNameQuantidade);
         this.totalText = findViewById(R.id.editTextNumberDecimalTotal);
 
+        //não editável
+        this.totalText .setEnabled(false);
+        this.totalText.setInputType(InputType.TYPE_NULL);
+
         //RadioGroup
         this.unidadeMedidaRadioGroup = findViewById(R.id.radioGroupUnidadeMedida);
 
@@ -43,11 +47,18 @@ public class MainActivity extends AppCompatActivity {
         //CheckBox
         this.favoritoCheckBox = findViewById(R.id.checkBoxFavorito);
 
-        this.popularSpinner();
+        //Events
+        //this.totalText.setOnClickListener(new AdapterView.OnItemClickListener());
+
+        this.popularSpinnerSetor();
 
     }
 
-    public void limparCampos(View view){
+    public void limpar(View view){
+        this.limparCampos();
+    }
+
+    private void limparCampos(){
         this.descricaoProdutoText.setText("");
         this.precoText.setText("");
         this.quantidadeText.setText("");
@@ -55,8 +66,7 @@ public class MainActivity extends AppCompatActivity {
         this.unidadeMedidaRadioGroup.clearCheck();
         this.setorSpinner.setSelection(0);
         this.favoritoCheckBox.setChecked(false);
-
-        //this.descricaoProdutoText.();
+        this.descricaoProdutoText.requestFocus();
     }
 
     public void calcularTotal(View View) {
@@ -65,34 +75,135 @@ public class MainActivity extends AppCompatActivity {
         BigDecimal price = BigDecimal.ZERO;
         BigDecimal somaTotal = BigDecimal.ZERO;
 
-        if (this.quantidadeText.getText().toString().trim() != null
-            && this.precoText.getText().toString().trim()  != null ) {
+        if (!this.quantidadeText.getText().toString().trim().equals("")
+            && !this.precoText.getText().toString().trim().equals("") ) {
             qtd =  Integer.parseInt(this.quantidadeText.getText().toString().trim());
             price =  new BigDecimal(this.precoText.getText().toString().trim());
             somaTotal = price.multiply(new BigDecimal(qtd));
-            this.totalText.setText(somaTotal.toString());
+            this.totalText.setText(limitarDuasDecimais(somaTotal.toString()).toString());
         }
     }
 
-    public void popularSpinner(){
-        List<String> listaSetor = new ArrayList<>(asList(
-                "Selecione o Setor do Produto",
-                "Higiene e limpeza",
-                "Frutas, verduras e legumes",
-                "Mercearia",
-                "Padaria",
-                "Enlatados",
-                "Cereais",
-                "Rotisseria",
-                "Açougue",
-                "Frios e laticínios",
-                "Adega e bebidas"
-        )); //Será implementado com a classe Setor
+    public void popularSpinnerSetor(){
 
+        //Será implementado com a classe Setor
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                                                            android.R.layout.simple_list_item_1,
-                                                           listaSetor);
+                                                getResources().getStringArray(R.array.setores));
         setorSpinner.setAdapter(adapter);
+    }
+
+    public boolean validaCampos(){
+        boolean checkCampos;
+        try {
+
+            //TextView
+            String descricaoProd = this.descricaoProdutoText.getText().toString().trim();
+
+            //menor que 3 caracteres
+            if (descricaoProd.length() < 3){
+                this.descricaoProdutoText.requestFocus();
+                throw new IllegalArgumentException(
+                            getString(R.string.descricao_do_produto) + " " +
+                            getString(R.string.minimo_tres_caracteres));
+            }
+
+            //limite que 100 caracteres - implementar info.
+            if (descricaoProd.length() > 50){
+                this.descricaoProdutoText.requestFocus();
+                throw new IllegalArgumentException(
+                        getString(R.string.descricao_do_produto) + " " +
+                                getString(R.string.maximo_cem_caracteres));
+            }
+
+            //Preco
+            String preco = this.precoText.getText().toString().trim();
+
+            if (preco.equals("")){
+                this.precoProduto = new BigDecimal(getString(R.string.zero));
+            }else{
+              this.precoProduto = new BigDecimal(preco);
+            }
+
+            //Quantidade
+            String quantProd = this.quantidadeText.getText().toString().trim();
+
+            if (quantProd.equals("")){
+                this.quantidadeProdutos = Integer.parseInt("0");
+            }else{
+                this.quantidadeProdutos = Integer.parseInt(quantProd);
+            }
+
+            if (this.quantidadeProdutos > 999){
+                this.quantidadeText.requestFocus();
+                throw new IllegalArgumentException(getString(R.string.quntidade_limite_999));
+            }
+
+            //RadioGroup
+            switch (this.unidadeMedidaRadioGroup.getCheckedRadioButtonId()){
+                case R.id.radioButtonUnidade:
+
+                    break;
+
+                case R.id.radioButtonPacote:
+
+                    break;
+
+                default:
+                throw new IllegalArgumentException(getString(R.string.selecione_unidade_medida));
+            }
+
+            //Spinner
+            String setor = (String) this.setorSpinner.getSelectedItem();
+
+            if (setor.equals(getString(R.string.selecione_setor_produto))) {
+                this.setorSpinner.hasFocus();
+                throw new IllegalArgumentException(getString(R.string.selecione_setor_produto));
+            }
+
+
+            //Favorito - Não é necessário adicionar validação
+
+            return checkCampos = true;
+
+        }catch(IllegalArgumentException e){
+            this.mostrarMensagem(e.getMessage());
+            return checkCampos = false;
+        }
+    }
+
+    public void salvarProduto(View view){
+        this.quantidadeProdutos = Integer.parseInt(getString(R.string.zero));
+        this.precoProduto = BigDecimal.ZERO;
+
+        if (this.validaCampos()) {
+            this.descricaoProdutoText.getText();
+            this.precoText.getText();
+
+            int trocarClasseQuantidade = this.quantidadeProdutos;
+
+            this.unidadeMedidaRadioGroup.getCheckedRadioButtonId();
+            String setor = (String) this.setorSpinner.getSelectedItem();
+            this.favoritoCheckBox.isChecked();
+            this.limparCampos();
+            this.mostrarMensagem(getString(R.string.produto_cadastrado));
+        }
+
+    }
+
+    private void mostrarMensagem(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+
+    public void tratarCampoPreco(View view){
+        String preco = this.precoText.getText().toString().trim();
+        if (!preco.equals("")){
+          this.precoText.setText(limitarDuasDecimais(preco).toString());
+        }
+    }
+
+    public BigDecimal limitarDuasDecimais(String value){
+       return new BigDecimal(value).setScale(2, BigDecimal.ROUND_UNNECESSARY);
     }
 
 }
